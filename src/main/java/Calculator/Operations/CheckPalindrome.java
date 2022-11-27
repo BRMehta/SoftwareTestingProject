6aac22fabdb06f5efd1102caf3c948713716bcf2
brm1996 <mehtabhargavr@gmail.com>
2022-11-27 12:11:56 +0530

    added check palindrome code and test cases

--- src/main/java/Calculator/Operations/CheckPalindrome.java
+++ src/main/java/Calculator/Operations/CheckPalindrome.java
@@ -1,10 +1,10 @@
 package Calculator.Operations;
 
 public class CheckPalindrome {
-    public static boolean is_palindrome(String str)
+    public static boolean isPalindrome(String str)
     {
         int i=0,j=str.length()-1;
-        while(i<=j)
+        while(i<j)
         {
             if(str.charAt(i)!=str.charAt(j))
                 return false;
--- src/test/java/Calculator/Operations/CheckPalindromeTest.java
+++ src/test/java/Calculator/Operations/CheckPalindromeTest.java
@@ -2,5 +2,52 @@ package Calculator.Operations;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 public class CheckPalindromeTest {
+    CheckPalindrome chk=new CheckPalindrome();
+
+    String str1="";
+    String str2="a";
+    String str3="1";
+    String str4="ab";
+    String str5="aa";
+    String str6="abc";
+    String str7="eye";
+    String str8="abba";
+    String str9="abbz";
+
+    @Test
+    public void isPalindrome()
+    {
+        boolean res=chk.isPalindrome(str1);
+        assertFalse(res==false);
+        assertEquals(true,res);
+
+        res=chk.isPalindrome(str2);
+        assertFalse(res==false);
+        assertEquals(true,res);
+
+        res=chk.isPalindrome(str3);
+        assertFalse(res==false);
+        assertEquals(true,res);
+
+        res=chk.isPalindrome(str4);
+        assertEquals(false,res);
+
+        res=chk.isPalindrome(str5);
+        assertEquals(true,res);
+
+        res=chk.isPalindrome(str6);
+        assertEquals(false,res);
+
+        res=chk.isPalindrome(str7);
+        assertEquals(true,res);
+
+        res=chk.isPalindrome(str8);
+        assertEquals(true,res);
+
+        res=chk.isPalindrome(str9);
+        assertEquals(false,res);
+    }
 }

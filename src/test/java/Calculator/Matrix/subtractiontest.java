7c01bf2d85c53ad5144eb4b80b6afe00d85cf9da
brm1996 <mehtabhargavr@gmail.com>
2022-11-27 17:24:09 +0530

    Matrix test cases

--- src/test/java/Calculator/Matrix/subtractiontest.java
+++ src/test/java/Calculator/Matrix/subtractiontest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class subtractiontest
@@ -23,24 +24,29 @@ public class subtractiontest
         int[][] response = sub.subtract(new int[][]{{10,8},{12,12}}, new int[][]{{5,2},{5,6}});
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                assertNotEquals(response[i][j], expresponse[i][j]);
+                assertEquals(response[i][j], expresponse[i][j]);
             }
         }
     }
 
     @Test
     void matSubtractNull() {
-        int[][] response = sub.subtract(new int[0][0], new int[0][0]);
-        assertNotEquals(1,response.length);
+        int[][] expresponse =new int[][]{{5,6},{7,6}};
+        int[][] response = sub.subtract(expresponse, new int[][]{{0,0},{0,0}});
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                assertEquals(expresponse[i][j],response[i][j]);
+            }
+        }
     }
 
     @Test
     void matSubtractOnce() {
-        int[][] expresponse =new int[][]{{2}};
+        int[][] expresponse =new int[][]{{0}};
         int[][] response = sub.subtract(new int[][]{{1}}, new int[][]{{1}});
         for (int i = 0; i < 1; i++) {
             for (int j = 0; j < 1; j++) {
-                assertNotEquals(response[i][j],expresponse[i][j]);
+                assertEquals(expresponse[i][j],response[i][j]);
             }
         }
     }

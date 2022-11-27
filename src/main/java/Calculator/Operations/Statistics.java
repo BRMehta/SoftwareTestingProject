e6dbc5ae4c0f087f3d3f34ad115b6604666b923f
brm1996 <mehtabhargavr@gmail.com>
2022-11-27 15:16:35 +0530

    Added empty array Exception handling for Statistical operations

--- src/main/java/Calculator/Operations/Statistics.java
+++ src/main/java/Calculator/Operations/Statistics.java
@@ -4,9 +4,22 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 
-public class Statistics {
-    public double findMean (double[] arr)
+class EmptyArrayException extends RuntimeException
+{
+    public EmptyArrayException(String str)
     {
+        super(str);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
+public class Statistics {
+    public double findMean (double[] arr) throws EmptyArrayException {
+        if(arr.length == 0)
+            throw new EmptyArrayException("Array is empty.");
         long count=0;
         double sum=0;
         for(double val:arr)
@@ -14,11 +27,14 @@ public class Statistics {
             count++;
             sum+=val;
         }
+
         return sum/count;
     }
 
     public double findMedian (double[] arr)
     {
+        if(arr.length == 0)
+            throw new EmptyArrayException("Array is empty.");
         Arrays.sort(arr);
         if(arr.length%2==1)
             return arr[arr.length/2];
@@ -28,12 +44,14 @@ public class Statistics {
 
     public double findMode (double[] arr)
     {
+        if(arr.length == 0)
+            throw new EmptyArrayException("Array is empty.");
         long maxFreq=-1;
         double ans = 0;
         HashMap<Double, Long> countMap = new HashMap<>();
         for(double val:arr)
         {
-            if(countMap.containsKey(val))
+            if(!countMap.containsKey(val))
             {
                 countMap.put(val,1L);
             }
@@ -52,6 +70,11 @@ public class Statistics {
                 maxFreq = (Long)mapElement.getValue();
                 ans = (Double)mapElement.getKey();
             }
+            else if(maxFreq == (Long)mapElement.getValue() && ans > (Double)mapElement.getKey())
+            {
+                ans=(Double)mapElement.getKey();
+            }
+
         }
         return ans;
     }
--- src/test/java/Calculator/Operations/StatisticsTest.java
+++ src/test/java/Calculator/Operations/StatisticsTest.java
@@ -1,41 +1,59 @@
 package Calculator.Operations;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StatisticsTest {
     Statistics stats=new Statistics();
     double[] arr1={1,2,3,3};
     double[] arr2={1.1,2.6,6.7};
     double[] arr3={0};
-    double[] arr4={-1,2,3,4,-54,5,-1};
+    double[] arr4={-1,2,3,4,-54,5,-1};//-54 -1 -1 2 3 4 5
     double[] arr5={};
 
     double res;
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
     @Test
     public void findMean()
     {
-        res= stats.findMean(arr1);
-        //System.out.println(res);
+        EmptyArrayException exception;
+
+        res = stats.findMean(arr1);
         assertEquals(2.25, res, 0.01);
 
-        res= stats.findMean(arr2);
+        res = stats.findMean(arr2);
         assertEquals(3.46666666667, res, 0.1);
 
-        res= stats.findMean(arr3);
+        res = stats.findMean(arr3);
         assertEquals(0, res, 0.01);
 
-        res= stats.findMean(arr4);
+        res = stats.findMean(arr4);
         assertEquals(-6.0, res, 0.01);
 
-        res= stats.findMean(arr5);
+
+        exception  = assertThrows(EmptyArrayException.class, new Executable() {
+
+            @Override
+            public void execute() throws Throwable {
+                res= stats.findMean(arr5);
+            }
+        });
+
+        assertEquals("Array is empty.",exception.getMessage());
     }
 
     @Test
     public void findMedian()
     {
+        EmptyArrayException exception;
+
         res= stats.findMedian(arr1);
         assertEquals(2.5, res, 0.01);
 
@@ -46,14 +64,24 @@ public class StatisticsTest {
         assertEquals(0, res, 0.01);
 
         res= stats.findMedian(arr4);
-        assertEquals(2.25, res, 0.01);
+        assertEquals(2, res, 0.01);
 
-        res= stats.findMedian(arr5);
+        exception  = assertThrows(EmptyArrayException.class, new Executable() {
+
+            @Override
+            public void execute() throws Throwable {
+                res= stats.findMedian(arr5);
+            }
+        });
+
+        assertEquals("Array is empty.",exception.getMessage());
     }
 
     @Test
     public void findMode()
     {
+        EmptyArrayException exception;
+
         res= stats.findMode(arr1);
         assertEquals(3, res, 0.01);
 
@@ -66,6 +94,14 @@ public class StatisticsTest {
         res= stats.findMode(arr4);
         assertEquals(-1, res, 0.01);
 
-        res= stats.findMode(arr5);
+        exception  = assertThrows(EmptyArrayException.class, new Executable() {
+
+            @Override
+            public void execute() throws Throwable {
+                res= stats.findMode(arr5);
+            }
+        });
+
+        assertEquals("Array is empty.",exception.getMessage());
     }
 }
